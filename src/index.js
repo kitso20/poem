d12549545fd31a76efe9084549a146f6972c3096
kitso918 <motlashupingkitso@gmail.com>
2025-01-09 07:41:43 +0200

    fixed the context

--- src/index.js
+++ src/index.js
@@ -12,17 +12,18 @@ function searchResults(event){
     event.preventDefault()
     console.log(`love`)
     let topicElement = document.querySelector(`#text`)
+    let poem = document.querySelector(`#poem`)
+    poem.classList.add(`poem`)
     let topic = topicElement.value
     let key = `313bd4cf07a141554b6c1ta1037bbboc`
     let prompt = `make poem about ${topic}`
-    let context = `make sure to make it less then 20 lines`
+    let context = `generate a less than 15 line poem and separate each line with a <br />, add <a><strong>Shecodes AI</strong></a? at the end`
     let APIurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`
 
     new Typewriter("#poem", {
-        strings: `wait`,
+        strings: `Generating Poem...`,
         autoStart: true,
-        cursor: "",
-        delay: 10,
+        delay: 100,
       });
       console.log(topic)
     axios.get(APIurl).then(write)
